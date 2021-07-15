5173532b721a896c413d12eabc3e52796469cd76
ELMoulaouiAnouar <elmoulaoui.anouar@gmail.com>
2021-07-15 11:30:47 -0400

    first commit

---
+++ App/data/data.js
@@ -0,0 +1,20 @@
+var quiz=''+
+'{"quiz":['+
+'{"id" : 0,"description":"Predict the output of the following JavaScript code.","question" '+
+':"&lt;script type=&#34;text/javascript&#34;&gt;<br/>a = 8 + &#34;8&#34;;<br/>document.write(a);<br/></script><br/>",'+
+'"answer"  :"88","option" :{"option1" : "16",'+
+'"option2" : "Compilation Error","option3" : "88","option4" : "Compilation Error"}}'+
+
+',{"id" : 1,"description":"Predict the output of the following JavaScript code.","question" : "&lt;script type=&#34;text/javascript&#34;&gt;<br/>var a=&#34;GeeksforGeeks&#34;;<br/>var x=a.lastIndexOf(&#34;G&#34;);<br/>document.write(x);<br/>&lt;/script&gt;","answer"  :"8","option" :{"option1" : "8","option2" : "0","option3" : "9","option4" : "Error"}}'+
+
+',{"id" : 2,"description":"Predict the output of the following JavaScript code.","question" : "&lt;script type=&#34;text/javascript&#34; language=&#34;javascript&#34;&gt;<br/>  <br/>var a = &#34;GeeksforGeeks&#34;;<br/>var result = a.substring(4, 5);<br/>document.write(result);<br/>  <br/>&lt;/script&gt;","answer"  :"s","option" :{"option1" : "sf","option2" : "ks","option3" : "s","option4" : "k"}}'+
+
+',{"id" : 3,"description":"Predict the output of the following JavaScript code.","question" : "&lt;script type=&#34;text/javascript&#34; language=&#34;javascript&#34;&gt;<br/>  <br/>var x=5;<br/>var y=6;<br/>var res=eval(&#34;x*y&#34;);<br/>document.write(res);<br/>  <br/>&lt;/script&gt;","answer"  :"30","option" :{"option1" : "&#34;30&#34;","option2" : "30","option3" : "5*6","option4" : "“5*6”"}}'+
+
+',{"id" : 4,"description":"How to write an ‘if’ statement for executing some code. ","question" : "If “i” is NOT equal to 5?","answer"  :"if(i!=5)","option" :{"option1" : "if(i&lt;&gt;5)","option2" : "if(i!=5)","option3" : "if i&lt;&gt;5","option4" : "if i!=5"}}'+
+
+',{"id" : 5,"description":"Predict the output on the console for the following JavaScript","question" : "&lt;script&gt;<br/>      <br/>    function geek() {<br/>    if(true) {<br/>        var a = 5;<br/>    }<br/>    document.write(a);<br/>}<br/>geek();                    <br/>  <br/>&lt;/script&gt;","answer"  :"5","option" :{"option1" : "Compilation Error","option2" : "5","option3" : "Nothing will be printed as output","option4" : "Runtime error"}}'+
+
+
+
+']}';
---
+++ App/js/main.js
@@ -0,0 +1,4 @@
+var btn_start = document.querySelector('.btn_start');
+btn_start.addEventListener("click",function(){
+     location.replace("question.html");
+});
